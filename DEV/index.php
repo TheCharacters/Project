98a8a38961482c61b9e19d0d1d6d0093372012c2
Justin Schaefer <jds_06@hotmail.com>
2013-10-21 21:14:02 -0500

    Basic Skeleton of the site.
    This should get us going. I put in some column divisions as place
    holders, because I don't have the sketch that William and Scott worked
    on.

---
+++ DEV/index.php
@@ -0,0 +1,46 @@
+<?php
+include("top.html");
+?>
+
+<h1>Welcome to the Characters Music Exchange!</h1>
+
+<!-- Container -->
+<div id='container'>
+<!--LOGIN SCRIPT -->
+<div id="login_button">
+    <script>
+function open_win(){
+    window.open("login.php");
+}
+</script>
+</div>
+<!--END OF LOGIN SCRIPT -->
+
+<!-- Navigation Menu(bar) -->
+<div id='nav_container'>
+    <ul>
+        <li><a href="url">What is TC Music Exhange</a></li>
+        <li><a href="url">Search Music</a></li>
+        <li><a href="url">Search Musicians</a></li>
+        <li><a href="url">FAQ</a></li>
+        <li><a href="url"> About Us</a></li>
+    </ul>    
+</div>
+<!-- End Navigation Menu -->
+
+<div class='left_col'>
+    
+</div>
+
+<div class='mid_col'>
+    
+</div>
+
+<div class='right_col'>
+    
+</div>
+
+</div> <!-- End of container -->
+    </body>
+    
+</html>
---
+++ DEV/login.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="Login" method="post">
+            E-Mail: <input type="text" name="email"><br>
+            Password: <input type="text" name="password"><br>
+            <input type='submit'>
+        </form>
+    </body>
+</html>
