491f6d69a09fb9fc7a1605daae05452d759dd35f
Justin Schaefer <jds_06@hotmail.com>
2013-10-23 11:04:31 -0500

    Comment here....

--- DEV/login.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form action="Login" method="post">
-            E-Mail: <input type="text" name="email"><br>
-            Password: <input type="text" name="password"><br>
-            <input type='submit'>
-        </form>
-    </body>
-</html>
