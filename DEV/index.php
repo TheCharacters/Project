c0fb915e5a7509176833d0b597038e224527c857
Justin Schaefer <jds_06@hotmail.com>
2013-10-27 21:09:52 -0500

    Login scripts and PHP
    Will need the database setup to link the new pages to. Configuration
    files are: fg_membersite.php and membersite_config.php.

--- DEV/index.php
+++ DEV/index.php
@@ -90,7 +90,23 @@
         <div class='backdrop'></div>
         <div class='box'>
             <div class='close'>x</div>
-            Lightbox Content Goes Here</div>
+            
+            <form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+                <fieldset >
+                <legend>Login</legend>
+                <input type='hidden' name='submitted' id='submitted' value='1'/>
+ 
+                <label for='username' >UserName*:</label>
+                <input type='text' name='username' id='username'  maxlength="50" />
+ 
+                <label for='password' >Password*:</label>
+                <input type='password' name='password' id='password' maxlength="50" />
+ 
+                <input type='submit' name='Submit' value='Submit' />
+ 
+                </fieldset>
+            </form>
+        </div>
         <!-- END OF LIGHTBOX DIVs -->
         
         
