d4afb1b27c48e64fdc5d18e2e2ebcd19df9e4f5a
Justin Schaefer <jds_06@hotmail.com>
2015-04-25 19:11:23 -0500

    Revert "Chug Chug Chugin away"
    This reverts commit cca253bf1ebc8eec61c6b9b60016e125aaf2148f.

---
+++ DEV/userhome.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>TC Music Exchange</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="CSS/style.css" type="text/css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="main">
+            <div id="banner">
+                <img src="images/banner.jpg" alt="Welcome!" />
+            </div>
+            <div id="navbar">
+                <div id="links">
+                    <a href="index.php">Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="construction.html">About Us&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="construction.html">Contact Us&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                </div>
+                <div id="navlog">
+                    <a href="construction.html">Sign Up&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="construction.html">Sign In     </a>
+                </div>
+            </div>
+            <div id="container1">
+                <h2>Top Songs:</h2>
+                <ol>
+                    <li> Song 1 </li>
+                    <li> Song 2 </li>
+                    <li> Song 3 </li>
+                    <li> Song 4 </li>
+                    <li> Song 5 </li>
+                    <li> Song 6 </li>
+                </ol>
+            </div>
+            <div id="container2">
+                <h2>Top Bands:</h2>
+                <ol>
+                    <li> VAR 1</li>
+                    <li> VAR 2</li>
+                    <li> VAR 3</li>
+                    <li> VAR 4</li>
+                    <li> VAR 5</li>
+                    <li> VAR 6</li>
+                </ol>
+            </div>
+            <div id="container4">
+                <h2>Browse Bands</h2>
+                (php loop here to look for bands and add them to the list. Can be refined with organization pramators such as alphabetical sort or # of songs.)
+            </div>
+            <div id="footer">
+                &copy; 2013 The Characters project. William Penn University
+            </div>
+            <a href="userprofile.php"> Profile Page. </a>
+        </div>
+    </body>
+</html>
